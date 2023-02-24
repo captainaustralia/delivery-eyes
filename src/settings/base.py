6682ed55bd9a6838d0518ca6af8a55688e3d3663
Sandro <physrow@gmail.com>
2023-02-24 17:51:30 +0400

    delivery-eyes-1: black reformat

--- src/settings/base.py
+++ src/settings/base.py
@@ -3,4 +3,9 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET", "test")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("TOKEN_EXPIRE_TIME", datetime.timedelta(minutes=30))
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get(
+    "ACCESS_TOKEN_EXPIRE_TIME", datetime.timedelta(minutes=30)
+)
+REFRESH_TOKEN_EXPIRE_MINUTES = os.environ.get(
+    "REFRESH_TOKEN_EXPIRE_TIME", datetime.timedelta(seconds=1)
+)
